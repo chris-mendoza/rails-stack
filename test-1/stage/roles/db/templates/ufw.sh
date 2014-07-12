252de6a5a85351e29f9652fb1a35dfa1f98b1add
root <ccmendoza89@gmail.com>
2014-07-12 04:14:58 -0500

    more db firewall changes

--- test-1/stage/roles/db/templates/ufw.sh
+++ test-1/stage/roles/db/templates/ufw.sh
@@ -4,4 +4,4 @@ ufw allow 22
 {% for host in groups['app'] %}
 ufw allow from {{ hostvars[host]['rax_addresses']['private'][0]['addr'] }} to any port 3306
 {% endfor %}
-ufw enable
+ufw --force enable
