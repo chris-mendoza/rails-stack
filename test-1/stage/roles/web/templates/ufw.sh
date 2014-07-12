ff0e0a400653a3bd3c00f0774a9336358c344221
root <ccmendoza89@gmail.com>
2014-07-12 05:23:53 -0500

    added firewall rules for web server

---
+++ test-1/stage/roles/web/templates/ufw.sh
@@ -0,0 +1,7 @@
+ufw allow 22,80/tcp
+{% for host in groups['app'] %}
+ufw allow from {{ hostvars[host]['rax_addresses']['private'][0]['addr'] }} to any port 3000
+ufw allow from {{ hostvars[host]['rax_addresses']['private'][0]['addr'] }} to any port 3001
+ufw allow from {{ hostvars[host]['rax_addresses']['private'][0]['addr'] }} to any port 3002
+{% endfor %}
+ufw --force enable
