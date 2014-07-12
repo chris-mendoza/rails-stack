b3488a44f317e29f161aef27dacce21ed11f384c
root <ccmendoza89@gmail.com>
2014-07-12 04:27:59 -0500

    reorganized configuration yml and updated prov script

--- test-1/stage/prov.sh
+++ test-1/stage/prov.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #This is the master provision script.
 
+#provision nodes
 ansible-playbook nodes/web.yml
 ansible-playbook nodes/app.yml
 ansible-playbook nodes/db.yml
+
+#configure nodes
+ansible-playbook -i config/rax.py config/all.yml
