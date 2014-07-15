b8116f2f01845a2c4eee4ba84a93d13498cfc1f0
root <ccmendoza89@gmail.com>
2014-07-15 06:11:26 -0500

    unicorn init script update

--- test-1/stage/config/roles/app/files/routes.rb
+++ test-1/stage/config/roles/app/files/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :blogs
   root 'blogs#index'
+  get 'links', to: 'blogs#links'
+  get 'contactus', to: 'blogs#contactus'
+  get 'upload', to: 'blogs#upload'
 end
